8ba3d8bb6c380d85e613285c82d46a30365e468b
Yoojia Chen <yoojiachen@gmail.com>
2017-10-15 17:36:57 +0800

    添加UART实现

--- AthenaZigBee/cc2530.h
+++ AthenaZigBee/cc2530.h
@@ -5,44 +5,59 @@
 #ifdef _WIN32
 
 // Pin Group
-extern int WIN32_P0 = 0;
-extern int WIN32_P1 = 0;
-extern int WIN32_P2 = 0;
+extern unsigned char WIN32_P0 = 0;
+extern unsigned char WIN32_P1 = 0;
+extern unsigned char WIN32_P2 = 0;
 #define P0 WIN32_P0
 #define P1 WIN32_P1
 #define P2 WIN32_P2
 
 // Pin IO Dir
-extern int WIN32_P0DIR = 0;
-extern int WIN32_P1DIR = 0;
-extern int WIN32_P2DIR = 0;
+extern unsigned char WIN32_P0DIR = 0;
+extern unsigned char WIN32_P1DIR = 0;
+extern unsigned char WIN32_P2DIR = 0;
 #define P0DIR WIN32_P0DIR
 #define P1DIR WIN32_P1DIR
 #define P2DIR WIN32_P2DIR
 
 // Pin Pull State
-extern int WIN32_P0INP = 0;
-extern int WIN32_P1INP = 0;
-extern int WIN32_P2INP = 0;
+extern unsigned char WIN32_P0INP = 0;
+extern unsigned char WIN32_P1INP = 0;
+extern unsigned char WIN32_P2INP = 0;
 #define P0INP WIN32_P0INP
 #define P1INP WIN32_P1INP
 #define P2INP WIN32_P2INP
 
 // Pin Intertuptors
-extern int WIN32_IEN0 = 0;
-extern int WIN32_IEN1 = 0;
-extern int WIN32_IEN2 = 0;
+extern unsigned char WIN32_IEN0 = 0;
+extern unsigned char WIN32_IEN1 = 0;
+extern unsigned char WIN32_IEN2 = 0;
 #define IEN0 WIN32_IEN0
 #define IEN1 WIN32_IEN1
 #define IEN2 WIN32_IEN2
-extern int WIN32_P0IEN = 0;
-extern int WIN32_P1IEN = 0;
-extern int WIN32_P2IEN = 0;
+extern unsigned char WIN32_P0IEN = 0;
+extern unsigned char WIN32_P1IEN = 0;
+extern unsigned char WIN32_P2IEN = 0;
 #define P0IEN WIN32_P0IEN
 #define P1IEN WIN32_P1IEN
 #define P2IEN WIN32_P2IEN
-extern int WIN32_PICTL = 0;
+extern unsigned char WIN32_PICTL = 0;
 #define PICTL WIN32_PICTL
+
+extern unsigned char WIN32_PERCFG = 0;
+#define PERCFG WIN32_PERCFG
+extern unsigned char WIN32_P0SEL = 0;
+#define P0SEL WIN32_P0SEL
+extern unsigned char WIN32_U0CSR = 0;
+#define U0CSR WIN32_U0CSR
+extern unsigned char WIN32_U0GCR = 0;
+#define U0GCR WIN32_U0GCR
+extern unsigned char WIN32_U0BAUD = 0;
+#define U0BAUD WIN32_U0BAUD
+extern unsigned char WIN32_UTX0IF = 0;
+#define UTX0IF WIN32_UTX0IF
+extern unsigned char WIN32_U0DBUF = 0;
+#define U0DBUF WIN32_U0DBUF
 
 #else
 
---
+++ AthenaZigBee/uart.c
@@ -0,0 +1,97 @@
+#include "cc2530.h"
+#include "uart.h"
+
+char _receivedBuff = 0;
+uint _receivedIndex = 0;
+
+// Check Command is AT
+uint _isATCommand() {
+	if (_receivedIndex == 0 && 'A' == _receivedBuff) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+
+	if (_receivedIndex == 1 && 'T' == _receivedBuff) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+
+	if (_receivedIndex == 2 && '+' == _receivedBuff) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+
+	return 1;
+}
+
+// Reset AT
+uint _resetAT() {
+	_receivedBuff = 0;
+	_receivedIndex = 0;
+	return 0;
+}
+
+#ifndef _WIN32
+#pragma vector = URX0_VECTOR
+__interrupt void UART0_ISR(void)
+{
+	// 清中断标志
+	URX0IF = 0;
+	_ReceivedTemp = U0DBUF;
+}
+
+#endif // !_WIN32
+
+// Init UART
+void uartInit() {
+	PERCFG = 0x00;       // 位置1 P0口
+	P0SEL = 0x3c;        // P0_2,P0_3,P0_4,P0_5用作串口,第二功能
+	P2DIR &= ~0XC0;      // P0 优先作为UART0 ，优先级
+
+	U0CSR |= 0x80;       // UART 方式
+	U0GCR |= 11;         // U0GCR与U0BAUD配合
+	U0BAUD |= 216;       // 波特率设为115200
+	UTX0IF = 0;          // UART0 TX 中断标志初始置位1  （收发时候）
+	U0CSR |= 0x40;       // 允许接收
+	IEN0 |= 0x84;        // 开总中断，接收中断
+}
+
+// Send Data
+void uartSend(pchar data, const uint dataSize) {
+	if (dataSize > 0) {
+		// Disabled Reciever when sending..
+		U0CSR &= ~0x40;
+		int i;
+		for (i = 0; i < dataSize; i++) {
+			U0DBUF = *data++;
+			while (UTX0IF == 0);
+			UTX0IF = 0;
+		}
+		// Enable Receiver
+		U0CSR |= 0x40;
+	}
+}
+
+// Receive data
+uint uartReceive(char* buff) {
+	// Check: AT+
+	if (_isATCommand()) {
+		if ('#' != _receivedBuff) {
+			buff[_receivedIndex++] = _receivedBuff;
+		}
+		else {
+			const uint len = _receivedIndex;
+			_resetAT();
+			return len;
+		}
+	}
+	else {
+		return _resetAT();
+	}
+}
